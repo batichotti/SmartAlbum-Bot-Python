c3d93e9282ab1aaa1d5b02b1cdf9a39df2790e37
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-20 10:57:38 -0300

    try something different

--- src/main/functions.py
+++ src/main/functions.py
@@ -1,14 +1,14 @@
 import pyautogui
-import time
+from time import sleep
 import os
 
 def abrir_programa(caminho_programa):
     try:
         pyautogui.hotkey("win", "r")
-        time.sleep(1)
+        sleep(1)
         pyautogui.write(caminho_programa)
         pyautogui.press("enter")
-        time.sleep(10)
+        sleep(10)
     except Exception as e:
         print(f"Erro ao abrir o programa: {e}")
 
@@ -24,32 +24,32 @@ def criar_novo_projeto(id):
         botao_criar_projeto = get_botao('./res/func/bt_novo_projeto.png')
         if botao_criar_projeto:
             pyautogui.click(botao_criar_projeto)
-            time.sleep(1)
+            sleep(1)
 
         botao_album = get_botao('./res/func/bt_album.png')
         if botao_album:
             pyautogui.click(botao_album)
-            time.sleep(2)
+            sleep(2)
 
         botao_personalizado = get_botao('./res/func/bt_personalizado.png') or get_botao('./res/func/bt_personalizado2.png')
         if botao_personalizado:
             pyautogui.click(botao_personalizado)
-            time.sleep(1)
+            sleep(1)
 
         botao_new_preset = get_botao('./res/func/bt_new_preset.png')
         if botao_new_preset:
             pyautogui.click(botao_new_preset)
-            time.sleep(1)
+            sleep(1)
             botao_23x60 = get_botao('./res/func/23x60.png')
             if botao_23x60:
                 pyautogui.click(botao_23x60)
-                time.sleep(1)
+                sleep(1)
         else:
             print("Botão de novo preset não encontrado, usando fallback.")
             pyautogui.press("enter")
-            time.sleep(1)
+            sleep(1)
             pyautogui.press("enter")
-            time.sleep(1)
+            sleep(1)
             pyautogui.write(f"{id}")
             pyautogui.press("enter")
 
@@ -63,28 +63,32 @@ def importar_imagens(images_folder):
         pyautogui.click(botao_import)
         pyautogui.hotkey("ctrl", "l")
         pyautogui.write(images_folder)
-        time.sleep(1)
+        pyautogui.press("right")
+        pyautogui.press("enter")
+        pyautogui.press("enter")
+        sleep(2)
         pyautogui.hotkey("ctrl", "a")
         pyautogui.press("enter")
 
         while get_botao('./res/func/load_importando_imagens.png') is not None:
-            print("Aguardando importação...")
-            time.sleep(10)
+            sleep(10)
+        return True
     else:
-        print("Botão de importar imagens não encontrado.")
+        return False
 
 def salvar_arquivos(caminho_exportar):
     botao_exportar = get_botao('./res/func/botao_exportar.png')
     if botao_exportar:
         pyautogui.click(botao_exportar)
-        time.sleep(2)
+        sleep(2)
         pyautogui.write(caminho_exportar)
         pyautogui.press('enter')
-        time.sleep(1)
+        sleep(1)
         pyautogui.hotkey('ctrl', 's')
         pyautogui.hotkey('alt', 'f4')
+        return True
     else:
-        print("Botão de exportar não encontrado.")
+        return False
 
 def processar_projetos(caminho_programa, pasta_entrada, pasta_saida):
     # abrir_programa(caminho_programa)
@@ -92,12 +96,16 @@ def processar_projetos(caminho_programa, pasta_entrada, pasta_saida):
         id_aluno = folder.replace(' ', '_')
 
         while not criar_novo_projeto(id_aluno):
-            time.sleep(1)
+            sleep(1)
 
         # if not criar_novo_projeto(id_aluno, pasta_saida):
         #     continue
 
-        time.sleep(2)
-        importar_imagens(f"{pasta_entrada}/{folder}")
-        salvar_arquivos(pasta_saida)
+        sleep(2)
+        while not importar_imagens(f"{pasta_entrada}/{folder}"):
+            sleep(1)
+        
+        while not salvar_arquivos(pasta_saida):
+            sleep(1)
+
         break
