70a026d5dfbe0033b1007d713ba09bcbe2133355
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-23 15:04:33 -0300

    Update gui.py
    Added a layer between the execution and the missthinking of the users

--- src/gui.py
+++ src/gui.py
@@ -85,7 +85,23 @@ class SmartAlbumApp:
             messagebox.showerror("Erro", "O campo 'Tempo em Minutos' deve conter um digito numerico como valor.")
             return
 
-        processar_projetos(caminho_pasta_entrada.replace("\\", "/"), caminho_pasta_salvamento.replace("\\", "/"), minutos, False)
+        texto_confirmacao = (
+            ">> Regras para o funcionamento adequado do programa:\n"
+            "1 - Nada de acentos nas palavras nos caminhos do computador/HD, verificar manualmente.\n"
+            "Atendidas as regras, pressione SIM para continuar a tarefa."
+        )
+
+        resposta = messagebox.askquestion(
+            "Confirmar Processamento", 
+            texto_confirmacao, 
+            icon="warning"
+        )
+
+        if resposta == "yes":
+            processar_projetos(caminho_pasta_entrada.replace("\\", "/"), caminho_pasta_salvamento.replace("\\", "/"), minutos, False)
+        else:
+            return
+
 
 def run_interface():
     root = tk.Tk()
